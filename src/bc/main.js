ea0526791c1d2270c3c737746641086a4e9c676d
Byoungchan Lee <dlqudcks002@naver.com>
2022-04-26 21:08:35 +0900

    bc real second commit

--- src/bc/main.js
+++ src/bc/main.js
@@ -5,67 +5,50 @@ tooleb.addEventListener('click',()=>{
     menu1.classList.toggle('active');
 });
 
-const check = document.querySelector('.checkbox');
+const check = document.querySelector('.result');
 const checklist = document.querySelector('.check_list');
 
 check.addEventListener('click', ()=>{
     checklist.classList.toggle('active');
+    check.classList.toggle('active');
 })
 
+const check2 = document.querySelector('.result2');
+const checklist2 = document.querySelector('.check_list2');
 
-let slides = document.querySelector('.slides');
-let slideImg = document.querySelectorAll('.slides li');
-currentIdx = 0;
+check2.addEventListener('click', ()=>{
+    checklist2.classList.toggle('active');
+    check2.classList.toggle('active');
+})
 
-slideCount = slideImg.length;
-prev = document.querySelector('.prev'); //이전 버튼 
-next = document.querySelector('.next'); //다음 버튼
+const check3 = document.querySelector('.result3');
+const checklist3 = document.querySelector('.check_list3');
 
-slideWidth = 300; //슬라이드이미지 넓이 
-slideMargin = 100;
-makeClone();
-// 처음이미지와 마지막 이미지 복사 함수 
-initfunction();
-//슬라이드 넓이와 위치값 초기화 함수 
-function makeClone() {
-    let cloneSlide_first = slideImg[0].cloneNode(true);
-    let cloneSlide_last = slides.lastElementChild.cloneNode(true);
-    slides.append(cloneSlide_first);
-    slides.insertBefore(cloneSlide_last, slides.firstElementChild);
-}
-function initfunction() {
-    slides.style.width = (slideWidth + slideMargin) * (slideCount + 2) + 'px';
-    slides.style.left = -(slideWidth + slideMargin) + 'px';
-} next.addEventListener('click', function () {
-    //다음 버튼 눌렀을때 
-    if (currentIdx <= slideCount - 1) {
-        //슬라이드이동 
-        slides.style.left = -(currentIdx + 2) * (slideWidth + slideMargin) + 'px'; slides.style.transition = `${0.5}s ease-out`;
-        //이동 속도 
+check3.addEventListener('click', ()=>{
+    checklist3.classList.toggle('active');
+    check3.classList.toggle('active');
+})
+
+let i = 0;
+document.querySelector('.prev').addEventListener('click',()=>{
+    if(i !=0){
+    i+=100;
+    document.querySelector('.container').style.transform = 'translate('+ i +'vw)';
+    }else{
+    i=-200;
+    document.querySelector('.container').style.transform = 'translate('+ i +'vw)';
     }
-    if (currentIdx === slideCount - 1) {
-        //마지막 슬라이드 일때 
-        setTimeout(function () {
-            //0.5초동안 복사한 첫번째 이미지에서, 진짜 첫번째 위치로 이동 
-            slides.style.left = -(slideWidth + slideMargin) + 'px'; slides.style.transition = `${0}s ease-out`;
-        }, 500); currentIdx = -1;
-    } currentIdx += 1;
-});
-prev.addEventListener('click', function () { //이전 버튼 눌렀을때 
-    console.log(currentIdx); if (currentIdx >= 0) {
-        slides.style.left = -currentIdx * (slideWidth + slideMargin) + 'px';
-        slides.style.transition = `${0.5}s ease-out`;
+})
+
+document.querySelector('.next').addEventListener('click',()=>{
+    if(i >-200){
+    i-=100;
+    document.querySelector('.container').style.transform = 'translate('+ i +'vw)';
+    }else{
+    i=0;
+    document.querySelector('.container').style.transform = 'translate('+ i +'vw)';
     }
-    if (currentIdx === 0) {
-        setTimeout(function () {
-            slides.style.left = -slideCount * (slideWidth + slideMargin) + 'px'; slides.style.transition = `${0}s ease-out`;
-        }, 500);
-        currentIdx = slideCount;
-    } currentIdx -= 1;
-});
-
-
-
+})
 
 
 
